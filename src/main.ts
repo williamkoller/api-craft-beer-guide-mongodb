2edc392d3017d0bbd0f027909010df5c4956fe63
william koller <wkoller25@gmail.com>
2021-02-07 22:54:54 -0300

    feat: set ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT_SERVER, () =>
+    console.log(
+      `Server running on http://localhost:${process.env.PORT_SERVER}`,
+    ),
+  );
 }
 bootstrap();
