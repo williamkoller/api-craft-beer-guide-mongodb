1fe9926c15024b63375afc8abc9889433297b7df
william koller <wkoller25@gmail.com>
2021-02-07 22:43:36 -0300

    chore: add StyleModule and StyleSchema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { StyleModule } from '@/style/style.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI)],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), StyleModule],
   controllers: [],
   providers: [],
 })
---
+++ src/schemas/style.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({ _id: true })
+export class Style {
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: String })
+  category: string;
+
+  @Prop({ type: Date, default: Date.now })
+  createdAt: Date;
+
+  @Prop({ type: Date, default: Date.now })
+  updatedAt: Date;
+}
+
+export const StyleSchema = SchemaFactory.createForClass(Style);
