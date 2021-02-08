771b05be89a7ea1421665b8bb916e83a7b0c483d
william koller <wkoller25@gmail.com>
2021-02-07 22:56:19 -0300

    feat: set setGlobalPrefix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT_SERVER, () =>
     console.log(
       `Server running on http://localhost:${process.env.PORT_SERVER}`,
