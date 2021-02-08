ed487fdabe23a53a226e6d8bc1fd445c2e264948
william koller <wkoller25@gmail.com>
2021-02-07 22:46:27 -0300

    feat: set MongooseModule in StyleModule

--- src/style/style.module.ts
+++ src/style/style.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Style, StyleSchema } from '@/schemas/style.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Style.name,
+        schema: StyleSchema,
+      },
+    ]),
+  ],
+})
 export class StyleModule {}
