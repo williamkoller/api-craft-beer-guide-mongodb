69f50bce9087bb493bc0137a991a0f6e40481536
william koller <wkoller25@gmail.com>
2021-02-09 00:45:42 -0300

    refactor: add fields description and ref in Schema and Dto

--- src/schemas/style.schema.ts
+++ src/schemas/style.schema.ts
@@ -7,6 +7,12 @@ export type StyleDocument = Style & Document;
 export class Style {
   @Prop({ type: String, required: true })
   name: string;
+
+  @Prop({ type: String, required: true })
+  description: string;
+
+  @Prop({ type: String, required: true })
+  ref: string;
 }
 
 export const StyleSchema = SchemaFactory.createForClass(Style);
--- src/style/dtos/add-style.dto.ts
+++ src/style/dtos/add-style.dto.ts
@@ -4,4 +4,12 @@ export class AddStyleDto {
   @IsString()
   @IsNotEmpty({ message: 'This field cannot be empty.' })
   name: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'This field cannot be empty.' })
+  description: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'This field cannot be empty.' })
+  ref: string;
 }
